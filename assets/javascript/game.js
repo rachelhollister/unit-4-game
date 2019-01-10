7fac867796b871a58403aebd971020003db115a2
Rachel <10523424+rachelhollister@users.noreply.github.com>
2019-01-10 02:23:10 -0800

    cleaned up code and format

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,88 @@
-//my global variables 
-var wins = 0;
-var losses = 0;
-var guessesLeft = 5;
-var wrongGuesses = [];
-var computerChoice = "";
-var userChoice = "";
+var random_result;
+var lost = 0;
+var win = 0;
+var previous = 0;
 
-//computer will pick a random letter
-function computerMakesChoice() {
-    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var randomIndex = Math.floor(Math.random () * alphabet.length);
-//the var is the random letter picked by computer
-    computerChoice = alphabet[randomIndex];
-    console.log('computerChoice', computerChoice);
-}
+//begin game
+var resetAndStart = function () {
 
-// setting a function to reset game	
-function gameReset(){
-    guessesLeft = 5;
-    wrongGuesses = [];
-    computerMakesChoice();
-}
+//clears
+$(".robots").empty();
 
-//player selects a letter of the alphabet 
-    document.onkeyup = function(event) {
-        //translates to userChoice
-        userChoice = event.key;
-        // var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        console.log(userChoice);
-        //if user guesses right letter, WIN
-        //reset guesses left, wrong guesses, and randomize computer choice
-        if(userChoice === computerChoice){
-            wins++;
-            alert("You got lucky this time!");
-            document.querySelector('#wins').innerHTML = "Wins: " + wins;
-            console.log('wins', wins);
-            console.log(computerChoice);
-            gameReset();
-        }	
-        else if(guessesLeft === 0){
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("You will never be a true psychic as I am!");
-            console.log('losses', losses);
-            console.log('guessesLeft', guessesLeft);
-            console.log(computerChoice);
-            gameReset();
-        }
-        // if not, guesses left decreases
-        // push current user choice to array of wrongguesses
-        else {
-            guessesLeft--;
-            wrongGuesses.push(userChoice);
-            document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
-            document.querySelector('#wrongGuesses').innerHTML = "Letters used: " + wrongGuesses;
-            console.log('wrongGuesses', wrongGuesses);
-            console.log('userChoice', userChoice);
-            console.log('guessesLeft', guessesLeft);
-            console.log(computerChoice);
-        }}
+//images array 
+var images = [
+    'http://unrestrictedstock.com/wp-content/uploads/free-stock-vector-illustration-set-robots-robot-drawing-cartoon-technology-machine03.jpg',
+    'http://unrestrictedstock.com/wp-content/uploads/free-stock-vector-illustration-set-robots-robot-drawing-cartoon-technology-machine05.jpg',
+    'http://unrestrictedstock.com/wp-content/uploads/free-stock-vector-illustration-set-robots-robot-drawing-cartoon-technology-machine02.jpg',
+    'http://unrestrictedstock.com/wp-content/uploads/free-stock-vector-illustration-set-robots-robot-drawing-cartoon-technology-machine01.jpg'];
 
-        gameReset();
+//display a random result generator number at the start of the game, from 19-120
+random_result = Math.floor(Math.random() * 101 ) + 19; 
+
+//display solution number for player each game
+$(".result").html('Generate: ' + random_result);
+
+//create loop for robots, running 4x with each time generating a new number
+for(var i = 0; i < 4; i++){
+    var random = Math.floor(Math.random() * 11 ) + 1;
     
+    //data attr to refresh values
+    var robot = $("<div>");
+        robot.attr({
+            "class": 'robot',
+            "data-random": random 
+        });
+
+        //plug in var images
+        robot.css({
+            "background-image":"url('" + images[i] + "')",
+            "background-size":"contain",
+            "background-repeat":"no-repeat"
+        });
+
+        // robot.html(random); commented out since used for building the game. displays each new value generated.
+
+    $(".robots").append(robot);
+}
+
+$(".previous").html("Robo Count: ", previous);
+
+}
+
+resetAndStart();
+
+//run function for on clicks and results
+$(document).on('click', ".robot", function() {
+
+    var num = parseInt($(this).attr('data-random'));
+
+    previous += num;
+
+    $(".previous").html("Robo Count: " + previous);
+
+    console.log(previous);
+
+    //alert loss
+    if(previous > random_result){
+       lost++;
+       alert("You lost!!");
+       $(".lost").html("Defeat: " + lost);
+
+       previous = 0;
+
+       resetAndStart();
+    }
+    //alert win
+    else if(previous === random_result){
+        win++;
+        alert("You win!!");
+
+        $(".win").html("Victory: " + win);
+
+        previous = 0;
+        
+        resetAndStart();
+    }
+
+});
+
